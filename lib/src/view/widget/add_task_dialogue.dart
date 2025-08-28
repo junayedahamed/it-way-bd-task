71597d2393094adcaeb02fb6f23c6102ac09f88f
junayedahamed <junayedahamed660@gmail.com>
2025-08-29 04:39:32 +0600

    form validation added

--- lib/src/view/widget/add_task_dialogue.dart
+++ lib/src/view/widget/add_task_dialogue.dart
@@ -5,30 +5,65 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class AddTaskDialogue extends StatelessWidget {
   AddTaskDialogue({super.key});
   final controller = TextEditingController();
+  final _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text("Add Task"),
-      content: TextField(
-        controller: controller,
-        decoration: const InputDecoration(hintText: "Enter task title"),
+    return Form(
+      key: _key,
+      child: Dialog(
+        child: SizedBox(
+          width: 300,
+          height: 250,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text("Add Task"),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextFormField(
+                    controller: controller,
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Please enter a valid title";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Enter task title",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Cancel"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          context.read<TaskProvider>().addTask(controller.text);
+                          Navigator.pop(context);
+                          _key.currentState!.reset();
+                        }
+                      },
+                      child: const Text("Add"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text("Cancel"),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            if (controller.text.isNotEmpty) {
-              //adding task to addTask Method
-              context.read<TaskProvider>().addTask(controller.text);
-              Navigator.pop(context);
-            }
-          },
-          child: const Text("Add"),
-        ),
-      ],
     );
   }
 }
